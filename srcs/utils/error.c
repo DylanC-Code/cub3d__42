acf923aa51d0ea7c4aebfc9c30bbc759351ae462
DylanC-Code <dylanc.code@gmail.com>
2025-07-25 22:05:22 +0200

    feat(error): error_exit implemented

--- srcs/utils/error.c
+++ srcs/utils/error.c
@@ -6,7 +6,7 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 10:45:15 by dcastor           #+#    #+#             */
-/*   Updated: 2025/07/25 11:03:14 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/07/25 21:50:37 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,11 @@ void	ft_error(char *msg)
 	ft_putstr_fd("Error: ", STDERR_FILENO);
 	ft_putstr_fd(msg, STDERR_FILENO);
 	ft_putstr_fd("\n\n", STDERR_FILENO);
+}
+
+void	exit_error(char *msg, t_game *game)
+{
+	(void)game;
+	ft_error(msg);
+	exit(EXIT_FAILURE);
 }
